0aadb0256c52c1da40aab88dba67cff685de57ea
Himanshu Palve <himanshupalve1999@gmail.com>
2018-03-17 16:11:35 +0530

    edit shared preferrences

--- app/src/main/java/com/example/himanshupalve/pictcanteen/MainActivity.java
+++ app/src/main/java/com/example/himanshupalve/pictcanteen/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.himanshupalve.pictcanteen;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,13 +17,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 public class MainActivity extends AppCompatActivity {
 
     private final static int NUM_LIST_ITEMS =10;
     private RecyclerView mMenu;
     private MenuAdapter mAdapter;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPreferences=getDefaultSharedPreferences(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this,LoginActivity.class);
@@ -35,24 +40,25 @@ public class MainActivity extends AppCompatActivity {
         mAdapter =new MenuAdapter(NUM_LIST_ITEMS,getNames());
         mMenu.setAdapter(mAdapter);
     }
+
     private ArrayList<String> getNames()
     {
         ArrayList<String> mArrayList=new ArrayList<>();
         AssetManager assetManager=getAssets();
         try {
             InputStream inputStream = assetManager.open("itemsnames.txt");
-            /*
-             * Use the following object to implement FastDictionary
-             */
             BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
             String line = null;
             while((line = in.readLine()) != null) {
                 String word = line.trim();
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putString(word,"0");
+                editor.apply();
                 mArrayList.add(word);
             }
         } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(this,"Could not load dictionary",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Could not load menu",Toast.LENGTH_LONG).show();
         }
         return mArrayList;
     }
