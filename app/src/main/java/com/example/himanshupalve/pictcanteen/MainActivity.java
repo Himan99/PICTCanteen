e186256d35464c29cd5364d2de235bb8637ac311
Himanshu Palve <himanshupalve1999@gmail.com>
2018-03-17 12:15:42 +0530

    Add Bottom navigation and fragments

--- app/src/main/java/com/example/himanshupalve/pictcanteen/MainActivity.java
+++ app/src/main/java/com/example/himanshupalve/pictcanteen/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.himanshupalve.pictcanteen;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.firebase.FirebaseApp;
 
@@ -16,6 +21,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_item1:
+                                selectedFragment = BreakfastFragment.newInstance();
+                                break;
+                            case R.id.action_item2:
+                                selectedFragment = LunchFragment.newInstance("","");
+                                break;
+                            case R.id.action_item3:
+                                selectedFragment = CartFragment.newInstance("","");
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, BreakfastFragment.newInstance());
+        transaction.commit();
     }
 
 }
